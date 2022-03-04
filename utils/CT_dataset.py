be90a79436190696326f2c240b8517406d6c94b8
guoyankai <392759421@qq.com>
2022-03-04 16:14:21 +0800

    2022年3月4日:修改2D CT 数据处理

--- utils/CT_dataset.py
+++ utils/CT_dataset.py
@@ -8,30 +8,37 @@
 
 from torch.nn import functional as F
 import os
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from collections import Counter
 from config import args
 import logging
 from PIL import Image
-from torch.utils.data import Dataset, SubsetRandomSampler
+from torch.utils.data import Dataset, SubsetRandomSampler, DataLoader
 # torchvision中的数据增强
-from torchvision.transforms import Compose, Resize, ToTensor, Normalize, RandomResizedCrop, CenterCrop, RandomHorizontalFlip
+from torchvision.transforms import Compose, Resize, ToTensor, Normalize, RandomResizedCrop, CenterCrop, \
+    RandomHorizontalFlip
 
-class BasicDataset():
+
+class BasicDataset(Dataset):
     """二维CT的数据"""
+
     def __init__(self, data_path, scale=1):
         self.images_dirs = os.path.join(data_path, "images")
         self.masks_dirs = os.path.join(data_path, "masks")
         self.scale = scale
-        assert 0<scale<=1, "Scale must between 0 and 1"
+        assert 0 < scale <= 1, "Scale must between 0 and 1"
 
         self.image_names = os.listdir(self.images_dirs)
         logging.info(f"Creating dataset with {len(self.images_dirs)} examples.")
-        self.transform  = Compose([
-            Resize(0.5),
-            RandomHorizontalFlip(0.5), # 以概率0.5随机水平翻转。
-            ToTensor(), # 归一化为：将取值范围[0,255]的Image图像或(H,W,C)的array ===>【C,H,W】取值范围[0,1.0]的float tensor
+
+        self.transform = Compose([
+            # Resize(0.5),
+            # RandomHorizontalFlip(0.5), # 以概率0.5随机水平翻转。
+            ToTensor(),  # 归一化为：将取值范围[0,255]的Image图像或(H,W,C)的array ===>【C,H,W】取值范围[0,1.0]的float tensor
             # Normalize(mean=, std=)  # 这里是标准化，是原始数据的均值和方差
         ])
-
 
     def __getitem__(self, item):
         name = self.image_names[item]
@@ -39,22 +46,95 @@ class BasicDataset():
         mask_path = os.path.join(self.masks_dirs, name)
         image = Image.open(image_path)
         mask = Image.open(mask_path)
-        print("image:")
-        print(type(image))
-        print(image)
 
-        print("mask:")
-        print(type(mask))
+        assert image.size == mask.size, \
+            f"Image and mask {name} should have the same size, but are {image.size} and {mask.size}"
 
+        if self.transform:
+            image = self.transform(image)
+        mask_array = np.array(mask)
+        # mask_array = np.expand_dims(mask_array, axis=2)
+        # mask_array = mask_array.transpose((2,0,1))
 
-        return 0
+        return {"image": image, "mask": torch.from_numpy(mask_array), "name": name.split(".")[0]}
 
     def __len__(self):
         return len(self.image_names)
 
 
+def split_data_val(dataset, args, shuffle=True):
+    print("total sample:", len(dataset))
+    valid_rate = args.valid_rate
+    data_size = len(dataset)
+    indices = list(range(data_size))  # 生成索引
+    split = int(np.floor(valid_rate * data_size))  # np.floor返回不大于输入参数的最大整数
+    if shuffle:
+        np.random.seed(args.seed)
+        np.random.shuffle(indices)  # 根据随机种子打散索引
+    train_indices, val_indices = indices[split:], indices[:split]
+
+    # 生成数据采样器和加载器
+    train_sample = SubsetRandomSampler(train_indices)
+    val_sample = SubsetRandomSampler(val_indices)
+    print(f" train sample: {len(train_indices)},  val sample: {len(val_indices)}")
+    return train_sample, val_sample
+
+
+from config import args
+
 if __name__ == "__main__":
     # 测试数据
-    data = BasicDataset(args.data_path)
-    print(len(data))
-    print(data[0])
+    dataset = BasicDataset(args.data_path)
+    train_sample, val_sample = split_data_val(dataset, args)
+    train_loader = DataLoader(dataset, batch_size=args.batch_size, sampler=train_sample)
+    val_loader = DataLoader(dataset, batch_size=args.batch_size, sampler=val_sample)
+
+    for data in (train_loader):
+        print(data["image"].shape)
+        print(data["mask"].shape)
+        break
+
+    # print(data[214]["mask"].shape)
+    # img = np.array(data[214]["mask"].detach())
+    # print(Counter(img.ravel()))
+
+    # transform = Compose([
+    #     # Resize(),
+    #     # RandomHorizontalFlip(0.5),  # 以概率0.5随机水平翻转。
+    #     ToTensor(),  # 归一化为：将取值范围[0,255]的Image图像或(H,W,C)的array ===>【C,H,W】取值范围[0,1.0]的float tensor
+    #     # Normalize(mean=0.5, std=0.5)  # 这里是标准化，是原始数据的均值和方差
+    # ])
+    # image  = Image.open(r"D:\code\data\cbct\label\1\images\001_215.png")
+    # print(image)
+    # w, h = image.size
+    # pil_image = image.resize((512,512))
+    # image_array = np.array(pil_image)
+    # print(image_array.shape)
+    # image_array = np.expand_dims(image_array, axis=2)
+    # print(image_array.shape)
+    # img_trans = image_array.transpose((2, 0, 1))
+    # print(img_trans.shape)
+    #
+    # print(img_trans.dtype)
+    #
+    # img_trans.astype(float)
+    # print(img_trans.dtype)
+    #
+    # img_tensor = torch.from_numpy(img_trans)
+    # print(img_tensor)
+
+    # image_array = np.array(image)
+    # print(image_array)
+    # print("mean:",np.mean(image_array), "std:",np.std(image_array))
+    # print(image_array.shape)
+    #
+    #
+    # image_normal = norm_img(image_array)
+    # print("norm:image, mean:", np.mean(image_normal), "norm_image std:", np.std(image_normal))
+    # print(Counter(image_normal.ravel()))
+    #
+    #
+    # image_trans = transform(image)
+    # print(type(image_trans))
+    # print(image_trans.shape)
+    # print(Counter(np.array(image_trans.detach()).ravel()))
--- utils/common.py
+++ utils/common.py
@@ -12,3 +12,11 @@ def print_models(net):
         num_params+=param.numel()
     print(net)
     print("模型的参数量:", num_params)
+
+def norm_img(image_array):
+    # 归一化像素值到（0，1）之间，且将溢出值取边界值
+    # image = (image - MIN_BOUND) / (MAX_BOUND - MIN_BOUND)
+    normal_image = (image_array - np.min(image_array)) / (np.max(image_array) - np.min(image_array))
+    normal_image[normal_image > 1] = 1.
+    normal_image[normal_image < 0] = 0.
+    return normal_image

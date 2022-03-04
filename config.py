841ca6d4e9adcf769a82c566fcd98036a0daba91
guoyankai <392759421@qq.com>
2022-03-04 12:53:43 +0800

    2022年3月3日：修改配置文件

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ parser.add_argument("--n_threads", type=int, default=0, help="number of threads"
 parser.add_argument("--seed", type=int, default=2022, help="random seed")
 
 # 各种文件存储路径
-parser.add_argument("--data_path", default=r"D:\code\data\cbct\label", help="data path")
+parser.add_argument("--data_path", default=r"D:\code\data\cbct\label\1", help="data path")
 parser.add_argument("--test_data", default=r"D:\code\data\cbct\label", help="test data path")
 parser.add_argument("--save_path", default="./experiments", help="save model path")
 
@@ -24,5 +24,11 @@ parser.add_argument("--n_labels", type=int, default=17, help="number of classes"
 parser.add_argument("--valid_rate", type=float, default=0.2, help="验证集划分率")
 parser.add_argument("--batch_size", type=int, default=8, help="batch size")
 
+# 训练过程的参数
+parser.add_argument("--epochs", type=int, default=100, help="number of epochs to train")
+parser.add_argument("--lr", type=float, default=0.001,help="learning rate")
+parser.add_argument("--early-stop", type=int, default=30, help="early stopping")
+
+
 args = parser.parse_args()
-print(args)
+# print(args)
--- utils/CT_dataset.py
+++ utils/CT_dataset.py
@@ -7,22 +7,54 @@
 # @software: PyCharm
 
 from torch.nn import functional as F
+import os
+from config import args
+import logging
+from PIL import Image
+from torch.utils.data import Dataset, SubsetRandomSampler
+# torchvision中的数据增强
+from torchvision.transforms import Compose, Resize, ToTensor, Normalize, RandomResizedCrop, CenterCrop, RandomHorizontalFlip
 
-class CTdataset():
-    def __int__(self):
-        self.a = 1
+class BasicDataset():
+    """二维CT的数据"""
+    def __init__(self, data_path, scale=1):
+        self.images_dirs = os.path.join(data_path, "images")
+        self.masks_dirs = os.path.join(data_path, "masks")
+        self.scale = scale
+        assert 0<scale<=1, "Scale must between 0 and 1"
+
+        self.image_names = os.listdir(self.images_dirs)
+        logging.info(f"Creating dataset with {len(self.images_dirs)} examples.")
+        self.transform  = Compose([
+            Resize(0.5),
+            RandomHorizontalFlip(0.5), # 以概率0.5随机水平翻转。
+            ToTensor(), # 归一化为：将取值范围[0,255]的Image图像或(H,W,C)的array转换为【C,H,W】取值范围[0,1.0]的float tensor
+            # Normalize(mean=, std=)  # 这里是标准化，是原始数据的均值和方差
+        ])
 
 
     def __getitem__(self, item):
+        name = self.image_names[item]
+        image_path = os.path.join(self.images_dirs, name)
+        mask_path = os.path.join(self.masks_dirs, name)
+        image = Image.open(image_path)
+        mask = Image.open(mask_path)
+        print("image:")
+        print(type(image))
+        print(image)
+
+        print("mask:")
+        print(type(mask))
+
 
         return 0
 
-    def len(self):
-        return 1
+    def __len__(self):
+        return len(self.image_names)
 
 
-
-if __name__ =="__main__":
+if __name__ == "__main__":
     # 测试数据
-    data = CTdataset()
-
+    data = BasicDataset(args.data_path)
+    print(len(data))
+    print(data[0])
